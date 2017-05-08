6c289bfed23a627add21a2aac8d841b938f4c820
vfabrichnov <zorumnipa@gmail.com>
2017-05-08 22:32:18 +0300

    lab 2

--- lab1.py
+++ lab1.py
@@ -1,4 +1,6 @@
 import logging
+
+import math
 import matplotlib.pyplot as plt
 import numpy as np
 
@@ -7,16 +9,18 @@ __author__ = 'vadim'
 logger = logging.getLogger(__name__)
 
 
-def main(multiplier, module, init_value):
+def main(multiplier, module, init_value, y):
     array_z, rnd_numbers = calculate_rnd_numbers(init_value, module, multiplier)
     print('Random numbers: %s' % rnd_numbers)
     print('Array Z: %s' % array_z)
     dispersion, expectation = calculate_expect_and_dispersion(array_z, module)
     print('Math Expectation: %s' % expectation)
     print('Dispersion: %s' % dispersion)
-    draw_graphics(array_z)
+    # draw_graphics(array_z)
     rkor2, rkor10 = calculate_rkor(array_z)
     print('Correlation Coefficient 2: %s\nCorrelation Coefficient 10: %s' % (rkor2, rkor10))
+    # exponential_distribution(array_z, y, module)
+    erlang_distribution(array_z, y, module, k=9)
 
 
 def calculate_rnd_numbers(init_value, module, multiplier):
@@ -48,7 +52,7 @@ def calculate_expect_and_dispersion(array_z, module):
 def draw_graphics(array_z):
     area = 20
     colors = np.random.rand(len(array_z))
-    plt.scatter(array_z, range(0,len(array_z)), s=area, c=colors)
+    plt.scatter(array_z, range(0, len(array_z)), s=area, c=colors)
     plt.show()
     plt.hist(array_z, 10)
     plt.show()
@@ -59,7 +63,7 @@ def calculate_rkor(array_z):
     rkor10_arr = []
     for i, z in enumerate(array_z):
         try:
-            rkor2_arr.append(z*array_z[i+2])
+            rkor2_arr.append(z * array_z[i + 2])
         except IndexError:
             break
         try:
@@ -67,16 +71,63 @@ def calculate_rkor(array_z):
         except IndexError:
             pass
 
-    rkor2 = (12.0/(577.0-2.0)*sum(rkor2_arr)) - 3.0
-    rkor10 = (12.0/(577.0-10.0)*sum(rkor2_arr)) - 3.0
+    rkor2 = (12.0 / (577.0 - 2.0) * sum(rkor2_arr)) - 3.0
+    rkor10 = (12.0 / (577.0 - 10.0) * sum(rkor2_arr)) - 3.0
     return rkor2, rkor10
 
 
+'''
+LAB 2
+'''
+
+
+def exponential_distribution(array_z, y, module):
+    Xi = []
+    for z in array_z:
+        Xi.append(math.e ** (-y * z))
+    M = 1 / float(y)
+    D = 1 / (float(y) ** 2)
+    T = (module - 1)
+    exponential_expect = sum(Xi) / T
+    M_ex_Xi = []
+    for x in Xi:
+        M_ex_Xi.append((exponential_expect - x) ** 2)
+    exponential_dispersion = sum(M_ex_Xi) / T
+    plt.plot(Xi, array_z, 'go')
+    plt.draw()
+    return Xi
+
+
+def erlang_distribution(array_z, y, module, k):
+    Xi = []
+    for z in array_z:
+        Xi.append(math.e ** (-k * z))
+    Mx_er = 1/float(y)
+    xi = []
+    for z in array_z:
+        xi.append(sum(array_z) - Mx_er * np.log10(z))
+    T = module - 1
+    M_ex = sum(Xi) / float(T)
+    k_diap = []
+    M_exp_Xi = []
+    for i, x in enumerate(Xi):
+        M_exp_Xi.append((M_ex - x) ** 2)
+        try:
+            k_diap.append(x * Xi[i + 10])
+        except IndexError:
+            continue
+    D_exp = sum(M_exp_Xi)/T
+    plt.plot(Xi, array_z, 'go')
+    plt.draw()
+
+
+
+
 if __name__ == '__main__':
-    input_values = raw_input('Enter multiplier, module and init_value:\n')
-    if not input_values:
-        main(337.0, 577, 1.0)  # default values
-        exit(0)
-    multiplier, module, init_value = input_values.split()
-    multiplier, module, init_value = float(multiplier), int(module), float(init_value)
-    main(multiplier, module, init_value)
+    # input_values = raw_input('Enter multiplier, module and init_value:\n')
+    # if not input_values:
+    main(337.0, 577, 1.0, 10)  # default values
+    exit(0)
+    # multiplier, module, init_value, y = input_values.split()
+    # multiplier, module, init_value, y = float(multiplier), int(module), float(init_value), int(y)
+    # main(multiplier, module, init_value, y)
